6897e77717808579a402a094e5e4caede274472b
Sstobo <seanstobo@gmail.com>
2017-11-01 13:42:45 -0700

    Webpack commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  entry: './src/scripts.js', 
+  entry: './src/main.js',
+
+  output: {
+     filename: './build/bundle.js'
+  },
+  module: {
+    
+    rules: [
+    // ...other loaders...
+    {
+      test: /\.(eot|svg|ttf|woff|woff2)$/,
+      use: [{
+        loader: 'file?name=public/fonts/[name].[ext]'
+      }]
+    },
+      {
+         test: /\.scss$/,
+         use: [
+           {
+             loader: 'style-loader'
+           },
+           {
+             loader: 'css-loader',
+             options: {
+               sourceMap: true
+             }
+           },
+           {
+             loader: 'sass-loader',
+             options: {
+              sourceMap: true
+            }
+           }
+         ]
+      },
+   ]
+ }
+};
